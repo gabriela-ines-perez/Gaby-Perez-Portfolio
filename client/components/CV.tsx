13391cde80a571b4e3e1cab336fd2ca80ece44dd
Gabriela Perez <gaby@perez.co.nz>
2023-07-20 14:11:45 +1200

    added projects

--- client/components/CV.tsx
+++ client/components/CV.tsx
@@ -1,8 +1,8 @@
 export default function CV() {
   return (
-    <p>
-      download my CV here -
-      <a href="/Gabriela Perez- CV - July:August 2023.pdf">-CV</a>
-    </p>
+    <a href="/Gabriela Perez- CV - July:August 2023.pdf" target="_blank">
+      {' '}
+      <button>Download CV</button>
+    </a>
   )
 }
---
+++ client/components/ProjectCard.tsx
@@ -0,0 +1,28 @@
+interface Project {
+  name: string
+  url: string
+  gitHub: string
+  id: number
+  description: string
+  coverImage: string
+}
+
+export default function ProjectCard({
+  name,
+  coverImage,
+  gitHub,
+  description,
+}: Project) {
+  return (
+    <>
+      <div className="project-card">
+        <img src={coverImage} className="card-cover-image" alt={description} />
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <a href={gitHub} target="_blank" rel="noreferrer">
+          <img className="icon" src="/github.png" alt="github logo" />
+        </a>
+      </div>
+    </>
+  )
+}
